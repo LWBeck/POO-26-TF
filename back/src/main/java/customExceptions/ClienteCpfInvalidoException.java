c2f51117309a099f7333e703c6de20a4c4a3327e
Eliza Müller <75331265+LizzieMuller@users.noreply.github.com>
2026-06-22 14:03:08 +0000

    CPF exception

---
+++ back/src/main/java/customExceptions/ClienteCpfInvalidoException.java
@@ -0,0 +1,6 @@
+package customExceptions
+
+public class ClienteCpfInvalidoException extends RuntimeException
+            public ClienteCpfInvalidoException(){
+                super("CPF é inválido! Cheque os caracteres.")
+            }
